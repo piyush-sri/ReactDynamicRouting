654bb55265ac0fbcf23c2cce18916517627ccd7b
Piyush Srivastava <67270567+piyush-sri@users.noreply.github.com>
2024-06-09 00:17:07 +0530

    ReactJs Dynamic Routing

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import Routing from "./routes/Routing";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <Routing />
+    </div>
+  );
+}
+
+export default App;
